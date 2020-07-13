5cdb32ed9faf6ccabf3b471233939dc1c0a0e846
carol <carol@DESKTOP-FBISCE1>
2020-07-13 16:19:16 -0300

    Mensagens

--- src/main/java/br/com/fiap/bot/telegram/impl/TesteBot.java
+++ src/main/java/br/com/fiap/bot/telegram/impl/TesteBot.java
@@ -1,9 +1,13 @@
 package br.com.fiap.bot.telegram.impl;
 
+import java.text.NumberFormat;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 import com.pengrad.telegrambot.TelegramBot;
@@ -12,18 +16,22 @@ import com.pengrad.telegrambot.model.request.ChatAction;
 import com.pengrad.telegrambot.request.GetUpdates;
 import com.pengrad.telegrambot.request.SendChatAction;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.request.SendPhoto;
 import com.pengrad.telegrambot.response.GetUpdatesResponse;
 
 import br.com.fiap.bot.telegram.service.ClimatempoService;
 
 public class TesteBot {
 	private static final String ENDLINE = System.getProperty("line.separator");
-	private static final String TOKEN_TELEGRAM = "1272262215:AAEsDyneOcuI4ZngPqM1iuM-HtvG6s0EZO0";
+	private static final String TOKEN_TELEGRAM = "1130341206:AAFoWwB4Vhcax1QFw2obA8Ox64j9vNEbR44";
+	private static Double VALOR_NOTA = 0.00;
 	private static TelegramBot bot;
 	private static int messageOffset = 0;
+	static Locale ptBr = new Locale("pt", "BR");
 
 	public static void main(String[] args) {
 		bot = new TelegramBot(TOKEN_TELEGRAM);
+
 		while (true) {
 			GetUpdates getUpdates = new GetUpdates().limit(100).offset(messageOffset);
 			GetUpdatesResponse updatesResponse = bot.execute(getUpdates);
@@ -33,18 +41,49 @@ public class TesteBot {
 				System.out.println(update.updateId() + " Recebeu: " + update.message().text());
 				String textoDigitado = update.message().text().toLowerCase();
 
-				if (textoDigitado.contains("/help") || textoDigitado.contains("/start")) {
+				// Mensagem de Digitando
+				bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+
+				switch (textoDigitado.toLowerCase()) {
+				case "/help":
+				case "/start":
 					executaJornadaHelpCommand(update);
-				}
-
-				if (textoDigitado.contains("/clima")) {
+					break;
+				case "/clima":
 					executaJornadaClimatempo(update);
-				}
-				
-			
-
-				if (textoDigitado.contains("/data")) {
+					break;
+				case "/data":
 					executaJornadaData(update);
+					break;
+				case "/cardapio":
+				case "cardapio":
+					executaJornadaCardapio(update);
+					break;
+				case "/frango":
+					executaJornadaSelecao(update, 20.00);
+					break;
+				case "/carne":
+					executaJornadaSelecao(update, 25.00);
+					break;
+				case "/cocacola":
+					executaJornadaSelecao(update, 5.00);
+					break;
+				case "/agua":
+					executaJornadaSelecao(update, 3.00);
+					break;
+				case "/comanda":
+					executaJornadaComanda(update, VALOR_NOTA);
+					break;
+				case "/entrega":
+					// Consultar informações de trânsito
+					break;
+				case "/retirar":
+					bot.execute(new SendMessage(update.message().chat().id(), "A FIAP Food agradece a preferência." + ENDLINE + ENDLINE + 
+							"Quando chegar no restaurante basta apresentar o código: 1234."));
+					break;
+				default:
+					bot.execute(new SendMessage(update.message().chat().id(), "Opção não encontrada!"));
+					break;
 				}
 			});
 		}
@@ -58,10 +97,28 @@ public class TesteBot {
 	}
 
 	private static void executaJornadaHelpCommand(Update update) {
-		String textoResposta = "/clima - Obt�m informa��es referente a temperatura atual" + ENDLINE
-				+ "/data - Obt�m informa��es do dia atual" + ENDLINE;
+		Date dt = new Date();
+		Calendar c = Calendar.getInstance();
+		c.setTime(dt);
+
+		int hours = c.get(Calendar.HOUR_OF_DAY);
+		String periodo = "";
+
+		if (hours >= 1 && hours <= 12) {
+			periodo = "Bom dia. ";
+		} else if (hours >= 12 && hours <= 18) {
+			periodo = "Boa tarde. ";
+		} else if (hours >= 21 && hours <= 24) {
+			periodo = "Boa noite. ";
+		}
+
+		String textoResposta = periodo + "Seja bem vindo ao restaurante Fiap Food!" + ENDLINE + ENDLINE
+				+ "Caso queira ver o cardápio digite /cardapio, quando o seu pedido estiver finalizado digite /comanda."
+				+ ENDLINE + ENDLINE + "Se precisar de ajuda digite /help." + ENDLINE + ENDLINE
+				+ "Se quiser informações sobre o clima, digite /clima.";
 
 		bot.execute(new SendMessage(update.message().chat().id(), textoResposta));
+		bot.execute(new SendPhoto(update.message().chat().id(), "https://i.imgur.com/b1dZTyM.png"));
 	}
 
 	private static void executaJornadaClimatempo(Update update) {
@@ -69,7 +126,8 @@ public class TesteBot {
 		String cidade = null;
 
 		bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
-		bot.execute(new SendMessage(update.message().chat().id(), "Digite a cidade desejada ou \"Brasil\" para obter um resumo"));
+		bot.execute(new SendMessage(update.message().chat().id(),
+				"Digite a cidade desejada ou \"Brasil\" para obter um resumo"));
 		GetUpdates getUpdates = new GetUpdates().limit(100).offset(messageOffset++);
 
 		while (cidade == null) {
@@ -87,6 +145,35 @@ public class TesteBot {
 					e.printStackTrace();
 				}
 			}
+		}
+	}
+
+	private static void executaJornadaCardapio(Update update) {
+		String cardapio = "Clique na opção desejada: " + ENDLINE + ENDLINE + "Comidas: " + ENDLINE
+				+ "Frango: 20,00 - /frango" + ENDLINE + "Carne: 25,00 - /carne" + ENDLINE + ENDLINE + "Bebidas: "
+				+ ENDLINE + "Coca-Cola: 5,00 - /cocacola" + ENDLINE + "Água: 3,00 - /agua" + ENDLINE + ENDLINE
+				+ "Depois que selecionar todos os itens, basta selecionar sua comanda digitando /comanda.";
+
+		bot.execute(new SendMessage(update.message().chat().id(), cardapio));
+	}
+
+	private static void executaJornadaSelecao(Update update, double valorPrato) {
+		VALOR_NOTA = VALOR_NOTA + valorPrato;
+		String valorTotalFormatado = NumberFormat.getCurrencyInstance(ptBr).format(VALOR_NOTA);
+		bot.execute(new SendMessage(update.message().chat().id(), "Seu pedido está em " + valorTotalFormatado + ENDLINE
+				+ ENDLINE + "Após selecionar tudo que deseja, basta solicitar sua comanda digitando /comanda."));
+	}
+
+	private static void executaJornadaComanda(Update update, double valorTotal) {
+		String valorTotalFormatado = NumberFormat.getCurrencyInstance(ptBr).format(VALOR_NOTA);
+
+		if (valorTotal <= 0.00) {
+			bot.execute(new SendMessage(update.message().chat().id(),
+					"Sua comanda ainda esta vazia, deseja ver o cardápio? Se sim, digite /cardapio."));
+		} else {
+			bot.execute(new SendMessage(update.message().chat().id(), "O seu pedido ficou em: " + valorTotalFormatado
+					+ ENDLINE + ENDLINE
+					+ "Caso o seu pedido seja para entrega, digite /entrega para calcularmos a distância, se for retirar no restaurante digite /retirar."));
 		}
 	}
 }
